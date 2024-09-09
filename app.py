719af9527d1371942eca70959d09fc08b70cc811
MagomedovMurad20 <magdev09@gmail.com>
2024-09-10 00:13:48 +0300

    #add прописал создание базы данных, добавление записей, получение записей

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request
+from models import Artist, Album, Song, db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# связываем приложение с базой данных
+
+db.init_app(app)
+
 
 @app.route('/')
 
@@ -32,6 +41,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
         return "вы вошли в систему как " + username
+
+
+@app.route('/songs')
+def songs():
+    songs_list = Song.query.all()
+    return render_template('songs.html', songs=songs_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=9999)
